a3bf5d55fc00f249ea9b1d7fb924f166d1ab4347
Akshay Chaudhary <akshay.nitmz010@gmail.com>
2025-06-22 12:19:00 +0530

    Completed two sum in C#

--- solutions/arrays/TwoSum.cs
+++ solutions/arrays/TwoSum.cs
@@ -1,1 +1,18 @@
+class TwoSum
+{
+   
+    public static int[] TwoSum(int[] nums, int target) {
 
+       Dictionary<int, int> map = new Dictionary<int, int>();
+      for(int i=0;i<nums.Length;i++)
+        {
+          int c = target - nums[i];
+          if (map.ContainsKey(c))
+              return new int[] { map[c], i };
+          else
+             map[nums[i]] = i;
+         }
+       return null;
+    }
+ }
+ 
